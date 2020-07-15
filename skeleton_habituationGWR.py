2f04cd12394ebdc02ace277d1ea9e072e6cc148e
Rucha Dave <ruchadave16@gmail.com>
2020-07-15 18:06:47 -0400

    Finished the steps

--- skeleton_habituationGWR.py
+++ skeleton_habituationGWR.py
@@ -16,14 +16,14 @@ import matplotlib.pyplot as plt
 ###############################
 # TODO: Set a variable called stv to 4, this will define 
 #        the weight of the connection from input to output
-# stv = 
+stv = 4
 
 dec=0.7 # set weight decrement for habituation
 
 pls=[0, 0, 1, 0, 0] # set up a pulse
 
 # TODO: then create a list of 6 pulses, called x, to use for input
-# x = 
+x = list(pls * 6)
 
 v = stv # Set connection weight to start weight value
 
@@ -34,18 +34,23 @@ v = stv # Set connection weight to start weight value
 # TODO: Find the length of the input list (x)
 #        and assign its value to a new variable, nTs
 #        (which tracks the number of Time Steps)
-# nTs = 
+nTs = len(x)
 
 # TODO: Create a numpy array named y, a 1D vector
 #        (row vector) padded with zeros, with one
 #        element per each time step
-# y = 
+y = np.zeros((1, nTs))
 
 # TODO: use a for-loop to iterate 
 #        through each time step in 
 #        the input series and calculate
 #        the output at each time step. Ex:
-# for ...
+i = 0
+for a in y:
+    y[0, i] = v * x[i]
+    if x[i] > 0: #some stimulation
+        v *= dec
+    i += 1
 
 #     then indent 4 spaces and write the equation that
 #     describes how each input value in the vector x is 
